8bc77f3b7b461d0fc3c3e012b182c41558035549
Shehzeen Ali <shehzeenali12@gmail.com>
2024-09-10 15:17:33 +0500

    add files and folder

---
+++ app.js
@@ -0,0 +1,204 @@
+// Chap 5
+// Q1
+var q1 = "Ques 1";
+document.write("<h3> " + q1 + "</h3>")
+var num1 = 3;
+var num2 = 5;
+var res = num1 + num2;
+document.write("The sum of 3 & 5 is " + res + "<br>");
+
+// Q2
+var q2 = "Ques 2";
+document.write("<h3> " + q2 + "</h3>")
+var num1 = 3;
+var num2 = 5;
+var res = num1 - num2;
+document.write("The sum of 3 & 5 is " + res + "<br>");
+
+var num1 = 3;
+var num2 = 5;
+var res = num1 * num2;
+document.write("The sum of 3 & 5 is " + res + "<br>");
+
+var num1 = 3;
+var num2 = 5;
+var res = num1 / num2;
+document.write("The sum of 3 & 5 is " + res + "<br>");
+
+var num1 = 3;
+var num2 = 5;
+var res = num1 % num2;
+document.write("The sum of 3 & 5 is " + res + "<br>");
+
+// Q3
+var des1 = "Ques 3";
+document.write("<h3> " + des1 + "</h3>")
+var variable = 12;
+document.write("The value of variable is " + variable + "<br>");
+var variable = 5;
+document.write("The Initial value of variable is " + variable + "<br>");
+var exp = ++variable;
+document.write("The value of variable after Increment is " + exp + "<br>");
+var exp2 = variable + 7;
+document.write("The value of variable after addition " + exp2 + "<br>");
+var exp3 = --exp2;
+document.write("The  value of variable after decrement is " + exp3 + "<br>");
+var exp4 = exp3 % 3;
+document.write("The remainder value of variable is " + exp4 + "<br>");
+
+// Q4
+var des2 = "Ques 4";
+document.write("<h3> " + des2 + "</h3>");
+var price = 600;
+var resu = 600 * 5;
+document.write("Total cost to buy 5 tickets to a movie is " + res + "<br>" + "<br>" + "<br>");
+
+// Q5
+var des3 = "Table of 4";
+document.write("<h3> " + des3 + "</h3>");
+var result = 4 * 1;
+document.write("4 * 1 = " + result + "<br>");
+var result = 4 * 2;
+document.write("4 * 2 = " + result + "<br>");
+var result = 4 * 3;
+document.write("4 * 3 = " + result + "<br>");
+var result = 4 * 4;
+document.write("4 * 4 = " + result + "<br>");
+var result = 4 * 5;
+document.write("4 * 5 = " + result + "<br>");
+var result = 4 * 6;
+document.write("4 * 5 = " + result + "<br>");
+var result = 4 * 7;
+document.write("4 * 5 = " + result + "<br>");
+var result = 4 * 8;
+document.write("4 * 5 = " + result + "<br>");
+var result = 4 * 9;
+document.write("4 * 5 = " + result + "<br>");
+var result = 4 * 10;
+document.write("4 * 5 = " + result + "<br>");
+
+// Q6
+var des4 = "Ques 4";
+document.write("<h3> " + des4 + "</h3>");
+var deg = 25;
+var fahrenheit = (deg * 9/5) + 32;
+document.write("25°C is " + fahrenheit + "<br>");
+var deg2 = 70;
+var Celsius = (deg2 - 32) * 5/9;
+document.write("70°C is " + Celsius);
+
+// Q7
+var des5 = "Shopping Cart";
+document.write("<h1> " + des5 + "</h1>");
+var rup = 650;
+document.write("Price of item 1 is " + rup + "<br>");
+var qua = 3;
+document.write("Quality of item 1 is " + qua + "<br>");
+var rup2 = 100;
+document.write("Price of item 2 is " + rup2 + "<br>");
+var qua = 3;
+document.write("Quality of item 2 is " + qua + "<br>");
+var shipcharge = 100;
+document.write("Ship Charges " + shipcharge + "<br>" + "<br>");
+var tot = 650 * 3 + 100 * 7 + 100;
+document.write("Total cost of your order is " + tot);
+
+// Q8
+var des6 = "Marks";
+document.write("<h1> " + des6 + "</h1>");
+var mrk1 = 980;
+document.write("Total marks: " + mrk1 + "<br>");
+var mrkOb = 804;
+document.write("Total marks: " + mrkOb + "<br>");
+
+// Q9
+var des7 = "Currency in PKR";
+document.write("<h1> " + des7 + "</h1>");
+var cur = 104.80 * 10 + 28 * 25;
+document.write("Total Currecy in PKR: " + cur);
+
+// Q10
+var des8 = "Calcutions";
+document.write("<h1> " + des8 + "</h1>");
+var Num = 477;
+var cal = Num + 5 * 10 / 2;
+document.write("Total Calculation is " + cal);
+
+// Q11
+var des9 = "Age Calcutor";
+document.write("<h1> " + des9 + "</h1>");
+var Cyear = 2016;
+document.write("Current Year: " + Cyear + "<br>");
+var Byear = 1992;
+document.write("Birth Year: " + Byear + "<br>");
+var age = Cyear - Byear;
+document.write("Your Age " + age + "<br>");
+var age2 = age - 1;
+document.write(" Or Your Age maybe " + age2);
+
+// Q12
+var des10 = "The Geometrizer";
+document.write("<h1> " + des10 + "</h1>");
+var rad = 20;
+document.write("Radius of a Circle: " + rad + "<br>");
+var CircumferenceRes = 2 * 3.142 * rad;
+document.write("Circumference of a Circle: " + CircumferenceRes + "<br>");
+var AreaRes = 3.142 * 400;
+document.write("Area of a Circle: " + AreaRes + "<br>" + "<br>");
+
+// Q13
+var favoriteSnack = "Chips";
+document.write("Favorite Snack " + favoriteSnack + "<br>");
+var currentage = 18;
+document.write("Current Age " + currentage + "<br>");
+var maximumage = 78;
+document.write("Maximum Age" + maximumage + "<br>");
+var estimatedamountperday = 4;
+document.write("Estimated Amount Per day " + estimatedamountperday + "<br>");
+var daysleft = (maximumage - currentage) * 365;
+document.write("Days left: " + daysleft + "<br>");
+var totSnacks = 4 * daysleft;
+document.write("Total Snacks you would eat for the rest of your life " + totSnacks + "<br>");
+
+// Chap 6 to 9
+// Q1
+var des11 = "Result";
+document.write("<h3> " + des11 + "</h3>");
+var a = 10;
+document.write("The value of a is: " + a + "<br>" + "<br>" + "<br>");
+var postIncrement = ++a;
+document.write("The value of ++a is: " + postIncrement + "<br>");
+a = 11;
+document.write("Now the value of a is: " + a + "<br>" + "<br>");
+var a = a++;
+document.write("The value of a++ is: " + a + "<br>");
+var preIncrement = a++;
+document.write("Now the value of a is: " + preIncrement + "<br>" + "<br>");
+var postDecrement = --a;
+document.write("The value of --a is: " + postDecrement + "<br>");
+a = 11;
+document.write("Now the value of a is: " + a + "<br>" + "<br>");
+var preDeccrement = a--;
+document.write("The value of a-- is: " + preDeccrement + "<br>");
+var preDeccrement = a--;
+document.write("Now the value of a-- is: " + preDeccrement + "<br>" + "<br>");
+
+// Q2
+var x = 2;
+var y = 1;
+document.write("x is " + x + "<br>");
+document.write("y is " + y + "<br>");
+var step1 = --x;
+document.write("--x = " + step1 + "<br>");
+var step2 = --x - --y;
+document.write("--x - --y = " + step2 + "<br>");
+var step3 = --x - --y + ++x;
+document.write("--x - --y + ++x = " + step3 + "<br>");
+var step4 = --x - --y + ++x  + y--;
+document.write("--x - --y + ++x - y-- = " + step4 + "<br>");
+var final = --x - --y + ++x  + y--;
+document.write("Result is " + final + "<br>");
+
+// Q3
+var userName = prompt ("Enter Your Name");
+var userName = prompt ("Hi! Welcome to JS world");
